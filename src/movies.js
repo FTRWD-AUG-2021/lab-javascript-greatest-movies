ea6c6a7983b2ae6c4548641730a604f561e713eb
Julian Phillips <julian@julianlp.com>
2021-08-24 09:26:13 -0600

    Class review

--- src/movies.js
+++ src/movies.js
@@ -29,13 +29,57 @@ return (scores/array.length).toFixed(2)
  }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() { }
+function dramaMoviesScore(array) { 
+  // Filter array to only include dramas
+  const dramas = array.filter(item => item.genre.includes('Drama'))
+  
+  // Check if there are any dramas
+  if (dramas.length === 0) {
+    return 0;
+  }
+
+  // Reduce drama scores
+  const totalScore = dramas.reduce((acc, curr) => {
+    return acc + (curr.score || 0);
+  }, 0)
+
+  // Divide the reduced scores by the amount of dramas
+  return parseFloat((totalScore / dramas.length).toFixed(2))
+  // return (totalScore / dramas.length).toFixed(2) This returns a string instead of a number
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() { }
+function orderByYear(movies) { 
+  // Set the rules for sorting
+  const moviesSort = (a, b) => {
+    // Sort by year in ascending order
+    if (a.year < b.year) return -1
+    if (a.year > b.year) return 1
+    // If the year is the same, sort by alphabetical order by title
+    if (a.year === b.year) {
+      if (a.title < b.title) return -1;
+      if (a.title > b.title) return 1;
+      if (a.title === b.title) return 0;
+    }
+  }
+
+  return movies.sort(moviesSort); 
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() { }
+function orderAlphabetically(array) {
+  // Order alphabetically by title
+  // const alphaSort = (a, b) => {
+  //   return a.title.localeCompare(b.title)
+  // }
+
+  // Return the first 20 titles if there at least 20
+  // return array.sort(alphaSort).slice(0,20)
+  
+  return array.sort((a, b) => {
+    return a.title.localeCompare(b.title)
+  }).slice(0,(array.length > 20 ? 20 : array.length)).map(movie => movie.title)
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() { }
